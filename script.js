825b2a89accd8c33fdda50de7c371439f45accd2
Gabriella Simon <gb@gabriellasimon.me>
2016-10-25 12:54:06 +0200

    Add weather datas and temp, add weather-icons library

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ $(document).ready(function() {
   if("geolocation" in navigator) {
 	navigator.geolocation.getCurrentPosition(function(position) {
 		console.log(position);
-    var city;
+
     $.ajax({
       url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '',
       dataType: 'json'
     })
     .done(function(data) {
-      city = data.results[0].address_components[2].long_name;
+      var city = data.results[0].address_components[2].long_name;
       $('.city').html(city);
+
     });
 
 
@@ -21,7 +22,12 @@ $(document).ready(function() {
       dataType: 'jsonp'
     })
     .done(function(data) {
-      console.log(data);
+      console.log(data.currently);
+      var weather = data.currently.summary;
+      $('.weatherDescription').html(weather + ' ' + data.currently.icon);
+      var temperatureF = data.currently.temperature;
+      $('h3 span.temperature').html(temperatureF + '℉');
+      console.log(temperatureF.toString());
     });
 	});
   }
