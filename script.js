ef3e44f5a8420ba88fd503ca01e9c531393b13bc
Gabriella Simon <gb@gabriellasimon.me>
2016-10-25 16:48:55 +0200

    Add jQuery UI, and slide-in

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
   // initializing material JS
   $.material.init();
-  // getting data from the browsers geolocation API
+  $( "div" ).effect( "slide", 800 );
+  var weatherIcons = {
+    'clear-day': 'wi-day-sunny',
+    'clear-night': 'wi-night-clear',
+    'rain': 'wi-rain',
+    'snow': 'wi-snow',
+    'sleet': 'wi-sleet',
+    'wind': 'wi-windy',
+    'fog': 'wi-fog',
+    'cloudy': 'wi-cloudy',
+    'partly-cloudy-day': 'wi-day-cloudy',
+    'partly-cloudy-night': 'wi-night-alt-cloudy'
+  };
+  // getting data from the browsers geolocation API (latitude, longitude)
   if("geolocation" in navigator) {
 	navigator.geolocation.getCurrentPosition(function(position) {
 		console.log(position);
 
+    // getting data from google's geocode API to determine the place from lat and long
     $.ajax({
       url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '',
       dataType: 'json'
@@ -16,7 +30,7 @@ $(document).ready(function() {
 
     });
 
-
+    // getting weather data from darksky.net API
     $.ajax({
       url: 'https://api.darksky.net/forecast/51c52962b36fc78232c5d78a3ba8e5e8/' + position.coords.latitude + ',' + position.coords.longitude + '?callback=?',
       dataType: 'jsonp'
