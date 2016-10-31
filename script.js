31c7945ac700faa93927b9d71eb8db6e8cde2eba
Gabriella Simon <gb@gabriellasimon.me>
2016-10-31 15:35:49 +0100

    Add alert if type form is empty

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ $(document).ready(function() {
       dataType: 'jsonp'
     })
     .done(function(data) {
+      console.log(data);
       var weather = data.currently.summary;
       var icon = data.currently.icon;
       temperatureF = data.currently.temperature;
@@ -56,7 +57,7 @@ $(document).ready(function() {
     // preloader finishes:
     $("#status").fadeOut();
     $("#preloader").fadeOut();
-    // getting data from google's geocode API to determine the place from lat and long
+    // getting data from google's geocode API to determine the city name from lat and long and dispay it
     $.ajax({
       url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '',
       dataType: 'json'
@@ -64,6 +65,7 @@ $(document).ready(function() {
     .done(function(data) {
       var city = data.results[0].address_components[2].long_name;
       $('.city').html(city);
+
     });
 
     // getting weather data from darksky.net API
@@ -71,6 +73,45 @@ $(document).ready(function() {
     weatherData(positionUrl);
 	  });
     }
+
+    // on click on the search icon getting city name and the corresponding weather
+    $('.input-group-btn').on('click', function() {
+      console.log($('#addon1').val());
+      var cityName = $('#addon1').val();
+      if (cityName === "") {
+          console.log('Please write a city name');
+          $('.city').html('');
+          $('.weatherDescription').html('');
+          $('.weatherIcon').html('<i class="wi wi-na icon"></i>');
+          $('.temperature').html('<i class="wi wi-na"></i>');
+          // adding alert 
+          $('.alert').removeClass('inactive');
+      } else {
+          $('.city').html(cityName);
+          // remove alert if there was from earlier.
+          $('.alert').addClass('inactive');
+
+      }
+
+      // getting the latitude and longitude of the city from input field
+      var cityURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + cityName + '';
+      $.ajax({
+        url: cityURL,
+        dataType: 'json'
+      })
+      .done(function(data) {
+        console.log(data);
+        if (data.status === "ZERO_RESULTS") {
+          console.log('Please write an existing city name');
+          $('.city').html('');
+        } else {
+          var cityLat = data.results[0].geometry.location.lat;
+          var cityLng = data.results[0].geometry.location.lng;
+          var cityurl = 'https://api.darksky.net/forecast/51c52962b36fc78232c5d78a3ba8e5e8/' + cityLat + ',' + cityLng + '?callback=?';
+          weatherData(cityurl);
+        }
+
+      });
 
   //$.ajax({
   //  url: 'http://api.sunrise-sunset.org/json?lat=' + position.coords.latitude + '&lng=' + position.coords.longitude + '&date=today',
@@ -90,24 +131,10 @@ $(document).ready(function() {
       }
     });
 
-  $('.input-group-btn').on('click', function() {
-    var cityName = $('#addon1').val();
-    $('.city').html(cityName);
-    // getting the latitude and longitude of the city from input field
-    var cityURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + cityName + '';
-    $.ajax({
-      url: cityURL,
-      dataType: 'json'
-    })
-    .done(function(data) {
-      var cityLat = data.results[0].geometry.location.lat;
-      var cityLng = data.results[0].geometry.location.lng;
-      var cityurl = 'https://api.darksky.net/forecast/51c52962b36fc78232c5d78a3ba8e5e8/' + cityLat + ',' + cityLng + '?callback=?';
-      weatherData(cityurl);
-    });
 
 
-  });
+
+  }); // if('geolocation' in navigator)
 
 
-});
+}); // document.ready
